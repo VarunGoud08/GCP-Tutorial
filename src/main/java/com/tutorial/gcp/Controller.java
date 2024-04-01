5d60db88c38bdcabbf9bd8ebec083ed6b3892cbb
VarunGoud08 <varungoud5520@gmail.com>
2024-04-01 12:41:26 +0530

    -second commit

---
+++ src/main/java/com/tutorial/gcp/Controller.java
@@ -0,0 +1,15 @@
+package com.tutorial.gcp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/GCP")
+public class Controller {
+
+    @GetMapping("/Home")
+    public String getGCPHome(){
+        return "Welcome to GCP Tutorial";
+    }
+}
---
+++ src/main/java/com/tutorial/gcp/GcpTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.tutorial.gcp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GcpTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GcpTutorialApplication.class, args);
+	}
+
+}
